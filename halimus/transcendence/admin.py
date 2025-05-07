5bb0e9755724ba52895afef216ce7271f2bddc7d
Ali Uçar <alucar@Ali-MacBook-Air.local>
2025-05-07 14:00:08 +0300

    finished version

--- halimus/transcendence/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import User# MatchHistory, BlockedList, FriendList
-
-admin.site.register(User)
-# admin.site.register(MatchHistory)
-# admin.site.register(BlockedList)
-# admin.site.register(FriendList)
